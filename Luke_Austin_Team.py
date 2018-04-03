487bc43f82cd3c5deeb98a7f910f5400bd73c665
AustinEngle <37628659+AustinEngle@users.noreply.github.com>
2018-04-03 11:48:38 -0400

    Completed Strategy
    Austin Engle, Luke Commiskey

--- Luke_Austin_Team.py
+++ Luke_Austin_Team.py
@@ -6,9 +6,9 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'Roblox_MLGs' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'Luke_Austin_Team' # Only 10 chars displayed.
+strategy_name = 'Alternating starting with b'
+strategy_description = 'bcbc'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
@@ -26,7 +26,12 @@ def move(my_history, their_history, my_score, their_score):
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
     
-    return 'c'
+    if len(my_history)%2 == 0:
+        return 'b'
+    else:
+        return 'c'
+    
+    
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
